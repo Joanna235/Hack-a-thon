3d6718c163c07fddc721e6222646c9a2d25d6a0e
clpan <pancgl@outlook.com>
2023-07-29 13:51:21 -0600

    backend function on portal 8800

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,24 @@ import express from "express"
 import mysql from "mysql"
 
 const app = express()
+
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "password",
-  database: "test",
+  database: "test"
 })
 
 app.get("/", (req, res) => {
-  res.send("Hello this is the backend of Charity food project!")
+  res.json("hello this is the backend of Charity food project!")
+})
+
+app.get("/", (req, res) => {
+  const q = "SELECT * FROM section1"
+  db.query(q, (err, result) => {
+    if (err) return res.json(err)
+    return res.json(result)
+  })
 })
 
 app.listen(8800, () => {
